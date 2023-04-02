690380e21f37d95acef00fba5a72e49b992ccbe6
Shweta Kohinkar <shwetakohinkar30@gmail.com>
2023-04-01 22:49:52 -0500

    isPalindrome code

---
+++ isPalindrome.java
@@ -0,0 +1,34 @@
+public class isPalindrome {
+
+    public static void main(String args[])
+    {
+
+        int[] arr = {3, 6, 5, 8, 10, 20, 15};
+
+        if(function(-121))
+        {
+            System.out.println("true");
+        }
+        else
+        {
+            System.out.println("false");
+        }
+    }
+
+        public static boolean function(int x) {
+
+            int temp = x, rev =0;
+
+            while(temp!=0)
+            {
+                int ld = temp%10;
+                rev = rev * 10 + ld;
+                temp = temp / 10;
+            }
+            if(x<0 && rev == x)
+            {
+                return false;
+            }
+            return (rev == x);
+        }
+    }
