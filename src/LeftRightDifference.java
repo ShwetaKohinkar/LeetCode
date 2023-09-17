88759b40586892453e02713f343d5912ea7f76e0
Shweta Kohinkar <shwetakohinkar30@gmail.com>
2023-09-17 11:27:14 -0500

    Adding new file LeftRightDifference

---
+++ src/LeftRightDifference.java
@@ -0,0 +1,31 @@
+public class LeftRightDifference {
+
+    public static void main(String[] args)
+    {
+        int[] strs = new int[]{8,5,2,1,3,4};
+
+
+    }
+    public static int[] leftRightDifference(int[] nums) {
+
+        int n = nums.length;
+        int[] leftSum = new int[n];
+        int[] rightSum = new int[n];
+        int[] answer = new int[n];
+        leftSum[0] = rightSum[n-1] = 0;
+        for(int i =0; i< n - 1; i++)
+        {
+            leftSum[i+1] = leftSum[i] + nums[i];
+        }
+        for(int i =n-1; i > 0; i--)
+        {
+            rightSum[i-1] = rightSum[i] + nums[i];
+        }
+        for(int i =0; i< n; i++)
+        {
+            answer[i] = Math.abs(leftSum[i] - rightSum[i]);
+        }
+        return answer;
+
+    }
+}
